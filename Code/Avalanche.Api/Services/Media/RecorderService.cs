346dc631b02f2c2bc39ef585099e7c3cbff26411
Sorey Garcia <sorey@mill5.com>
2020-10-29 15:18:52 -0500

    Removing code to mock recording service

--- Code/Avalanche.Api/Services/Media/RecorderService.cs
+++ Code/Avalanche.Api/Services/Media/RecorderService.cs
@@ -20,8 +20,6 @@ namespace Avalanche.Api.Services.Media
     {
         readonly IConfigurationService _configurationService;
 
-        public bool UseMocks { get; set; }
-
         public RecorderSecureClient RecorderClient { get; set; }
 
         public RecorderService(IConfigurationService configurationService, IGrpcClientFactory<RecorderClient> grpcClientFactory, ICertificateProvider certificateProvider)
@@ -31,25 +29,6 @@ namespace Avalanche.Api.Services.Media
             var hostIpAddress = _configurationService.GetEnvironmentVariable("hostIpAddress");
             var mediaServiceGrpcPort = _configurationService.GetEnvironmentVariable("mediaServiceGrpcPort");
 
-            UseMocks = true;
-
-            if (UseMocks)
-            {
-                Mock<RecorderClient> mockGrpcClient = new Mock<RecorderClient>();
-                var fakeCall = TestCalls.AsyncUnaryCall(Task.FromResult(new Empty()), Task.FromResult(new Metadata()), () => Status.DefaultSuccess, () => new Metadata(), () => { });
-                mockGrpcClient.Setup(mock => mock.StartRecordingAsync(Moq.It.IsAny<RecordMessage>(), null, null, CancellationToken.None)).Returns(fakeCall);
-                mockGrpcClient.Setup(mock => mock.StopRecordingAsync(Moq.It.IsAny<Empty>(), null, null, CancellationToken.None)).Returns(fakeCall);
-
-                var mockFactory = new Mock<IGrpcClientFactory<RecorderClient>>();
-                mockFactory.Setup(m => m.GetSecureClient(It.IsAny<string>(), It.IsAny<X509Certificate2>(), It.IsAny<X509Certificate2>(), It.IsAny<string>(), It.IsAny<List<Interceptor>>(), It.IsAny<List<Func<Metadata, Metadata>>>()))
-                        .Returns(mockGrpcClient.Object);
-
-                grpcClientFactory = mockFactory.Object;
-            }
-
-
-            //Client = ClientHelper.GetSecureClient<WebRtcStreamer.WebRtcStreamerClient>($"https://{hostIpAddress}:{mediaServiceGrpcPort}", certificate);
-            //RecorderClient = ClientHelper.GetInsecureClient<Recorder.RecorderClient>($"https://{_hostIpAddress}:{mediaServiceGrpcPort}");
             RecorderClient = new RecorderSecureClient(grpcClientFactory, hostIpAddress, mediaServiceGrpcPort, certificateProvider);
         }
 
