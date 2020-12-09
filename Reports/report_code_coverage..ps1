7635c0a11acb0d3f3841e9a49b67dae99011caad
Nazik Huq <nazikh@mill5.com>
2020-12-09 13:10:23 +0000

    added local code coverage report generator

---
+++ Reports/report_code_coverage..ps1
@@ -0,0 +1,21 @@
+$repo="avlanche-storage"
+$homepath=$env:HOMEPATH
+$report_path="$homepath\$repo\codecoverage\"
+$targetdir="$homepath\$report_path\coveragereport"
+
+Remove-Item $report_path -Recurse
+Remove-Item $targetdir -Recurse
+
+dotnet tool install dotnet-reportgenerator-globaltool --global --configfile NuGet.Config
+
+dotnet test ..\Code\Avalanche.Api.Tests\Avalanche.Api.Tests.csproj `
+ -c release  /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=$report_path\Api
+
+dotnet test ..\Code\Avalanche.Security.Server.Tests\Avalanche.Security.Server.Tests.csproj `
+ -c release  /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=$report_path\Security
+
+reportgenerator "-reports:$report_path\*.xml" "-targetdir:$targetdir" -reporttypes:Html
+
+start $targetdir\index.Html
+
+ 
