b0deb90a742055830b9cc5c646c48308256a5850
Gabriel Stern-Robbins <gabriel.robbins@olympus.com>
2020-12-16 18:24:51 -0500

    LibId formatting

--- Code/Avalanche.Api/Managers/Devices/DevicesManagerForRecordingCommands.cs
+++ Code/Avalanche.Api/Managers/Devices/DevicesManagerForRecordingCommands.cs
@@ -9,12 +9,16 @@ namespace Avalanche.Api.Managers.Devices
     {
         private async Task<CommandResponse> StartRecording(Command command)
         {
-            #warning TODO: determine sourcing of this info.
+#warning TODO: determine sourcing of this info.
             //NOTE: it seems a bit awkward for the UI/API to need to know and/or generate this? Especially "libId"? 
+
+            var now = DateTime.UtcNow;
+            var libId = $"{now.Year}_{now.Month}_{now.Day}T{now.Hour}_{now.Minute}_{now.Second}";
+
             var message = new RecordMessage
             {
-                LibId = string.Empty,
-                RepositoryId = string.Empty
+                LibId = libId,
+                RepositoryId = "cache"
             };
 
             await _recorderService.StartRecording(message);
@@ -29,12 +33,15 @@ namespace Avalanche.Api.Managers.Devices
 
         private async Task<CommandResponse> CaptureImage(Command command)
         {
+            var now = DateTime.UtcNow;
+            var libId = $"{now.Year}_{now.Month}_{now.Day}T{now.Hour}_{now.Minute}_{now.Second}";
+
             var message = new CaptureImageRequest()
             {
                 Record = new RecordMessage
                 {
-                    LibId = DateTime.Now.ToString("s"), // TODO: this is wrong and needs to come from the procedure
-                    RepositoryId = "TestRepository" // TODO: this is wrong and needs to come from the procedure
+                    LibId = libId, // TODO: this is wrong and needs to come from the procedure
+                    RepositoryId = "cache" // TODO: this is wrong and needs to come from the procedure
                 },
         };
             await _recorderService.CaptureImage(message);
