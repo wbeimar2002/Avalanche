3006919c9e8697edcb19c9f8a6f188ac2402a94b
Zac Cone-Roddy <zac.roddy@olympus.com>
2020-12-17 12:14:44 -0500

    fix demo file endpoint

--- Code/Avalanche.Api/Controllers/V1/FilesController.cs
+++ Code/Avalanche.Api/Controllers/V1/FilesController.cs
@@ -53,7 +53,7 @@ namespace Avalanche.Api.Controllers.V1
 
 #warning TODO: This is entirely wrong and intended only for a workflow demo. Remove.
         // Need to define and implement correct image retrieval patterns. Not in scope of current work, but the following is not at all correct.
-        [HttpGet]
+        [HttpGet("DemoGetImageFile")]
         [AllowAnonymous]
         public IActionResult DemoGetImageFile([FromQuery]string path)
         {
