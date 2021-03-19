924cbb3dc97a7412eb11746494e03fdd4f7309ba
Craig Bray <acraigbray@gmail.com>
2021-03-19 15:05:43 -0400

    Remove unused references from Security.Server Remove nonstandard dockerfile

--- Code/Avalanche.Security.Server/Program.cs
+++ Code/Avalanche.Security.Server/Program.cs
@@ -13,30 +13,6 @@ namespace Avalanche.Security.Server
 {
 	public class Program
 	{
-        /*public static void Main(string[] args)
-		{
-			var host = CreateHostBuilder(args).Build();
-
-			using (var scope = host.Services.CreateScope())
-			{
-				var services = scope.ServiceProvider;
-				var context = services.GetService<AppDbContext>();
-				var passwordHasher = services.GetService<IPasswordHasher>();
-				DatabaseSeed.Seed(context, passwordHasher);
-			}
-
-			host.Run();
-		}
-
-		public static IHostBuilder CreateHostBuilder(string[] args)
-		{
-			return Host.CreateDefaultBuilder(args)
-										.ConfigureWebHostDefaults(webBuilder =>
-										{
-											webBuilder.UseStartup<Startup>();
-										});
-		}*/
-
         public static void Main(string[] args)
         {
             var logFile = Environment.GetEnvironmentVariable("loggerFileName") ?? "avalancheapilogs.txt";
