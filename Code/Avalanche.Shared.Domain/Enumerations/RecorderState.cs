f213f3d4d1550b14fe2a4e9ebc114a37bd290024
soreygarcia <soreygarcia@gmail.com>
2021-04-19 16:44:15 -0500

    Fixing bug on Finish and Discard

---
+++ Code/Avalanche.Shared.Domain/Enumerations/RecorderState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avalanche.Shared.Domain.Enumerations
+{
+    public enum RecorderState
+    {
+        unknown = 0,
+        idle = 1,
+        proc_not_recording = 2,
+        proc_recording_mov = 3,
+        proc_recording_pm = 4,
+        proc_recording_mov_and_pm = 5,
+        proc_usb_exporting = 6,
+        proc_saving = 7,
+        proc_discarding = 8
+    }
+}
