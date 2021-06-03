18a38b07df13dd799b2d7aaaac2343fabace0eeb
Sorey Garcia <soreyg@mill5.com>
2021-06-03 11:20:06 -0500

    Adding field for sorting parameter

--- Code/Avalanche.Api/ViewModels/FilterViewModelBase.cs
+++ Code/Avalanche.Api/ViewModels/FilterViewModelBase.cs
@@ -8,6 +8,7 @@ namespace Avalanche.Api.ViewModels
     {
         public int Page { get; set; }
         public int Limit { get; set; }
+        public int SortBy { get; set; }
 
         public FilterViewModelBase()
         {
