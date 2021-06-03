62409df98dbe1e88e80f2eb03d61333b44e75cbb
Craig Bray <acraigbray@gmail.com>
2021-06-03 14:37:54 -0400

    Fix appsettings.nswag.json

--- Code/Avalanche.Shared.Domain/Models/DepartmentModel.cs
+++ Code/Avalanche.Shared.Domain/Models/DepartmentModel.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        // Does the API really care about IsNew?
         public bool? IsNew { get; set; }       
     }
 }
