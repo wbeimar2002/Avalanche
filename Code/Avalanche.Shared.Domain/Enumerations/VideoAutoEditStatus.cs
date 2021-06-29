4e63013bbc6466e3a3eb8db53a47982d35e7fb46
Sorey Garcia <soreyg@mill5.com>
2021-06-29 10:10:38 -0500

    Updating Search Filter with new field for Basic Search

---
+++ Code/Avalanche.Shared.Domain/Enumerations/VideoAutoEditStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avalanche.Shared.Domain.Enumerations
+{
+    public enum VideoAutoEditStatus
+    {
+        Pending,
+        InProgress,
+        Completed
+    }
+}
