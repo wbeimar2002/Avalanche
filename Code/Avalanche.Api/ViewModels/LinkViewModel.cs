d849a06ec769d612dd5a1837eac771795c44fc40
Sorey Garcia <soreyg@mill5.com>
2021-07-13 19:10:32 -0500

    Improvements to links between entities

--- Code/Avalanche.Api/ViewModels/LinkViewModel.cs
+++ Code/Avalanche.Api/ViewModels/LinkViewModel.cs
@@ -7,6 +7,7 @@ namespace Avalanche.Api.ViewModels
 {
     public class LinkViewModel
     {
+        public string ParentIdKey { get; set; }
         public string Key { get; set; }
         public string TranslationKey { get; set; }
     }
