bc15f297c2f4811af40bb854534898cfeb619cce
Sorey Garcia <soreyg@mill5.com>
2021-08-25 10:16:55 -0500

    Adding endpoints to get recorder configuration. Fixing Background Recorder behavior on Quick registration. Refactoring Backgroun recording changes on registration.

--- Code/Avalanche.Shared.Infrastructure/Enumerations/BackgroundRecordingMode.cs
+++ Code/Avalanche.Shared.Infrastructure/Enumerations/BackgroundRecordingMode.cs
@@ -3,6 +3,7 @@ namespace Avalanche.Shared.Infrastructure.Enumerations
     public enum BackgroundRecordingMode
     {
         StartImmediately = 0,
-        StartOnMediaCapture = 1
+        StartOnMediaCapture = 1,
+        AlwaysStartOnCapture = 2
     }
 }
