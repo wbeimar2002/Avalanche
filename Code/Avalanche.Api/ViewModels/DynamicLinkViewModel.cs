aa23d9a335eb6ec9e190a574679daca0a746ea32
Sorey Garcia <soreyg@mill5.com>
2021-09-01 16:22:58 -0500

    Refactor

--- Code/Avalanche.Api/ViewModels/DynamicLinkViewModel.cs
+++ Code/Avalanche.Api/ViewModels/DynamicLinkViewModel.cs
@@ -3,7 +3,7 @@ namespace Avalanche.Api.ViewModels
     public class DynamicLinkViewModel
     {
         public string ParentIdKey { get; set; }
-        public string? ParentIdName { get; set; }
+        public string? ParentNameKey { get; set; }
         public string Key { get; set; }
         public string? Metadata { get; set; }
         public string? Source { get; set; }
