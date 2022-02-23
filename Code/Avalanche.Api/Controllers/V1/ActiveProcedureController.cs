e5bc2e5c2b07928c519698d2b5bb88645ae73950
Miguel Quintero <miguelq@mill5.com>
2022-02-22 19:10:50 -0500

    Unnecessary code corrected

--- Code/Avalanche.Api/Controllers/V1/ActiveProcedureController.cs
+++ Code/Avalanche.Api/Controllers/V1/ActiveProcedureController.cs
@@ -293,7 +293,7 @@ namespace Avalanche.Api.Controllers.V1
             {
                 _logger.LogDebug(LoggerHelper.GetLogMessage(DebugLogType.Requested));
 
-                var procedure = await _activeProcedureManager.AllocateNewProcedure(registrationMode, patient).ConfigureAwait(false);
+                _ = await _activeProcedureManager.AllocateNewProcedure(registrationMode, patient).ConfigureAwait(false);
 
                 return Ok();
             }
