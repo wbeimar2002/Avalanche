08271a4087f0346568600324f1308f0cf00ff55b
Nazik Huq <nazikh@mill5.com>
2022-03-16 07:58:45 -0400

    Added assembly info file and update project file

--- Code/Avalanche.Shared.Infrastructure/properties/AssemblyInfo.cs
+++ Code/Avalanche.Shared.Infrastructure/properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Avalanche - Backup.Shared.Infrastructure")]
+[assembly: AssemblyTitle("Avalanche.Shared.Infrastructure")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Gyrus ACMI, Inc.")]
-[assembly: AssemblyProduct("Avalanche - Backup.Shared.Infrastructure")]
+[assembly: AssemblyProduct("Avalanche.Shared.Infrastructure")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
